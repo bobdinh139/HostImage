e182bdf68c20f0f70b51e3c9e38cdd07c60c71c5
e <49532012+bobdinh139@users.noreply.github.com>
2020-11-16 17:48:43 -0500

    Add files via upload

---
+++ noiseCancelSimple/MainDude.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JFrame;
+
+public class MainDude {
+
+	public static void main(String args[]) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+	     new MrMoneyMaker().makeNoisePlz();
+	}
+
+} 
